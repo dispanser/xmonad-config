a25c9ce446c4a1648dc7413bedba6fde38eab5f2
Thomas Peiselt <pi@kulturguerilla.org>
2021-11-06 07:48:10 +0100

    add hook for threema-web message tag

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -118,11 +118,12 @@ myManageHook = namedScratchpadManageHook scratchpads
   , appName   `endsWith`   "_org"                 --> doRectFloat centeredRect
   -- title: WM_NAME / _NET_WM_NAME
   , title      =?          "Slack Call Minipanel" --> doRectFloat (W.RationalRect (17/20) (9/10) (fullWidth / 5) (2*fullHeight / 18))
-  , title `startsWith` "Slack"                    --> addTagHook "m"
-  , title `startsWith` "Signal"                   --> addTagHook "m"
+  , title     `startsWith` "Slack"                --> addTagHook "m"
+  , title     `startsWith` "Signal"               --> addTagHook "m"
+  , className =?           "threema-web"          --> addTagHook "m"
   , className =?           "TelegramDesktop"      --> addTagHook "m"
   , className =?           "Franz"                --> addTagHook "m" >> doRectFloat centeredRect
-  , className  =?          "Pinentry"             --> doRectFloat smallCentered
+  , className =?           "Pinentry"             --> doRectFloat smallCentered
   , className =?           "Vimb"                 --> addTagHook "b"
   , className =?           "Firefox"              --> addTagHook "b"
   , className `startsWith` "Chromium"             --> addTagHook "b"
